0a8f68309ea0669c12efa8054cb8b9a9542fa4b8
bbzzyyczczeek <bbzzyyczczeek@interia.pl>
2023-03-01 18:04:18 +0100

    Initial commit

---
+++ src/main/java/com/example/eqipments/EqipmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.eqipments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqipmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqipmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eqipments/EqipmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eqipments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqipmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
